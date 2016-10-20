e11229de9e20e4b9d82637bde76c2ee65053b5f2
jozi-k <jozef.koval03@gmail.com>
2016-10-20 16:26:53 +0200

    Add windowed stream processing.

--- src/main/scala/test/AvroProducer.scala
+++ src/main/scala/test/AvroProducer.scala
@@ -1,5 +1,6 @@
 package test
 
+import java.time.{LocalDate, LocalDateTime, LocalTime, ZoneId}
 import java.util.{Properties, UUID}
 
 import org.apache.avro.Schema
@@ -8,16 +9,28 @@ import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 import scala.io.Source
+import scala.util.Random
 
 object AvroProducer {
   def main(args: Array[String]): Unit = {
-    val users = List(User(1, "Martin Odersky"), User(2, "Erik Meijer"), User(3, "Martin Odersky"))
+    val date = LocalDate.now()
     val producer = new KafkaProducer[String, GenericRecord](CommonProperties())
 
-    users.map(Record.create)
-      .foreach(rec => producer.send(rec))
+    while (true) {
+      val sum = Random.nextInt(2)
 
-    producer.close()
+      val time = if (sum == 0) {
+//        Change value here when you test it.
+        LocalTime.of(16, 20)
+      } else {
+        LocalTime.now()
+      }
+      val dt = LocalDateTime.of(date, time)
+      val user = User(sum, "Odersky", dt)
+      producer.send(Record.create(user))
+
+      Thread.sleep(1000L)
+    }
   }
 }
 
@@ -29,8 +42,6 @@ object CommonProperties {
     props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer")
     props.put("schema.registry.url", "http://localhost:8081")
     props.put("metadata.broker.list", "localhost:9092")
-    props.put("message.send.max.retries", "5")
-    props.put("request.required.acks", "-1")
     props.put("serializer.class", "kafka.serializer.DefaultEncoder")
     props.put("client.id", UUID.randomUUID().toString)
     props
@@ -39,12 +50,13 @@ object CommonProperties {
 
 object Record {
   val schema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/schema.avsc")).mkString)
-  val topic = "avro-users"
+  val topic = "avro-users-with-ts"
 
   def create(user: User): ProducerRecord[String, GenericRecord] = {
     val genericUser: GenericRecord = new GenericData.Record(schema)
     genericUser.put("sum", user.sum)
     genericUser.put("name", user.name)
+    genericUser.put("ts", user.dateTime.atZone(ZoneId.systemDefault()).toInstant.toEpochMilli)
     new ProducerRecord[String, GenericRecord](topic, genericUser)
   }
 }
--- src/main/scala/test/AvroStreams.scala
+++ src/main/scala/test/AvroStreams.scala
@@ -1,44 +1,49 @@
 package test
 
 import java.util.{Properties, UUID}
+import java.lang.{Long => JLong}
+import java.time.{Instant, LocalDateTime, ZoneId}
 
-import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
-import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder, KTable}
-import KeyValueImplicits._
+import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsConfig}
+import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder, TimeWindows}
 import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.processor.TimestampExtractor
 
-case class User(sum: Integer, name: String)
+case class User(sum: Integer, name: String, dateTime: LocalDateTime)
 
 object AvroStreams {
   def main(args: Array[String]): Unit = {
     val builder = new KStreamBuilder
 
-    val users: KStream[String, GenericRecord] = builder.stream("avro-users")
+    val users: KStream[String, GenericRecord] = builder.stream("avro-users-with-ts")
 
     val stringSerde = Serdes.String()
-    val intSerde = Serdes.Integer()
 
     val valuesMapped: KStream[String, User] = users.mapValues(createUser)
-    val names: KStream[String, Integer] = valuesMapped.map { (_, v) => (v.name, v.sum)}
-    val temp = names.through(stringSerde, intSerde, "avro-temp")
+    val names: KStream[String, Integer] = valuesMapped.map { (_, v) => new KeyValue(v.name, v.sum) }
+    val timeWindow =
+      TimeWindows.of("GeoPageViewsWindow", 5 * 1000L)
+        .advanceBy(5 * 1000L)
+        .until(15 * 1000L)
 
-    val agg: KTable[String, Integer] = temp.reduceByKey((s1, s2) => s1 + s2, "Aggregate")
-    agg.foreach((k,v) => println(s"key is : $k, value is $v"))
+    val counted = names.countByKey(timeWindow)
+    val str: KStream[String, String] = counted.toStream((k, _) => k.window.start.toString)
+      .mapValues(_.toString)
 
-    agg.to(stringSerde, intSerde, "avro-aggregate")
+    str.to(stringSerde, stringSerde, "window-key-count")
 
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092")
     props.put("schema.registry.url", "http://localhost:8081")
     props.put("metadata.broker.list", "localhost:9092")
-    props.put("message.send.max.retries", "5")
-    props.put("request.required.acks", "-1")
     props.put("serializer.class", "kafka.serializer.DefaultEncoder")
     props.put("client.id", UUID.randomUUID().toString)
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "avro-aggregation-scala-example")
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "avro-stream-windowing-scala-example")
     props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
     props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, classOf[GenericAvroSerde])
+    props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, classOf[EventTimestampExtractor])
 
     val stream = new KafkaStreams(builder, props)
     stream.start()
@@ -47,6 +52,16 @@ object AvroStreams {
   def createUser(r: GenericRecord): User = {
     val sum = r.get("sum").asInstanceOf[Integer]
     val name = r.get("name").toString
-    User(sum, name)
+    val ts = LocalDateTime.ofInstant(Instant.ofEpochMilli(r.get("ts").asInstanceOf[JLong]), ZoneId.systemDefault())
+    User(sum, name, ts)
+  }
+}
+
+class EventTimestampExtractor extends TimestampExtractor {
+  override def extract(record: ConsumerRecord[AnyRef, AnyRef]): Long = {
+    record.value
+      .asInstanceOf[GenericRecord]
+      .get("ts")
+      .asInstanceOf[JLong]
   }
 }
